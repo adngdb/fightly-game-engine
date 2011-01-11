2dac4e5fab92f3284161bab4124ceafada28fd27
Gaudebert Adrian <adrian.gaudebert@gmail.com>
2011-01-11 19:47:10 +0100

    Accept connections and write to clients.

--- dev/server/game-engine.js
+++ dev/server/game-engine.js
@@ -1,1 +1,28 @@
+var sys = require("sys"),
+    ws  = require('./vendor/node-websocket-server/lib/ws/server');
 
+function log(data){
+  sys.log("\033[0;32m"+data+"\033[0m");
+}
+
+var server = ws.createServer();
+server.listen(3400);
+
+server.addListener("connection", function(client)
+{
+    log("New connection: " + client.id);
+    server.send(client.id, "Welcome");
+
+    client.addListener("message", function(msg)
+    {
+        log("Message received: " + msg);
+        server.broadcast(msg);
+    });
+
+    client.addListener("close", function()
+    {
+        log("Connection closed by client");
+    });
+});
+
+log("Server created. Listening on port 3400. ");
