a676d455d9dd2ff1d28c7db2ea91bfc9bc48abcf
AdrianGaudebert <adrian@gaudebert.fr>
2011-01-13 16:25:43 +0100

    CommonJs spec, adding some functions.

--- dev/server/lib/game-engine.js
+++ dev/server/lib/game-engine.js
@@ -1,15 +1,30 @@
-var WGE = function () {
-    var components = {},
-        entities = {},
-        games = {};
+var sys = require('sys');
 
-    return {
-        this.log = function(data) {
-            sys.log("\033[0;32m"+data+"\033[0m");
-        }
+var components = {},
+    entities = {},
+    games = {};
 
-        this.init = function() {
-            this.log("GameEngine: init()");
-        }
+exports.log = function(data) {
+    sys.log("\033[0;32m"+data+"\033[0m");
+};
+var log = this.log;
+
+exports.WGE = function() {
+    this.init();
+};
+
+exports.WGE.prototype = {
+    init: function() {
+        log("GameEngine: init()");
+        return this;
+    },
+
+    start: function() {
+        log("GameEngine: start()");
+        return this;
+    },
+
+    getGames: function() {
+        return this.games;
     }
-}();
+}
