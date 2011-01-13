981b829f2983c24481f5e87db3186c3532c36d26
AdrianGaudebert <adrian@gaudebert.fr>
2011-01-13 16:25:16 +0100

    Launch the game-engine.

--- dev/server/server.js
+++ dev/server/server.js
@@ -1,28 +1,4 @@
-var sys = require("sys"),
-    ws  = require("websocket-server");
+var ge = require("./lib/game-engine.js"),
+    wge = new ge.WGE();
 
-function log(data){
-  sys.log("\033[0;32m"+data+"\033[0m");
-}
-
-var server = ws.createServer();
-server.listen(3400);
-
-server.addListener("connection", function(client)
-{
-    log("New connection: " + client.id);
-    server.send(client.id, "Welcome");
-
-    client.addListener("message", function(msg)
-    {
-        log("Message received: " + msg);
-        server.broadcast(msg);
-    });
-
-    client.addListener("close", function()
-    {
-        log("Connection closed by client");
-    });
-});
-
-log("Server created. Listening on port 3400. ");
+wge.start();
