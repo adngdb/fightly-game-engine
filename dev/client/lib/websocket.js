18b5cce043df6dda0980a2fb1e63e372a9da0d6c
ducnguyen272 <ducnguyen272@gmail.com>
2011-01-14 10:08:15 +0100

    just pour tester

--- dev/client/lib/websocket.js
+++ dev/client/lib/websocket.js
@@ -4,15 +4,20 @@ window.onload = function() {
         alert('Votre navigateur ne supporte pas les web sockets');
     else
     {
-        socket.load();
+        client.load();
+	document.getElementById("idLogIn").onclick = logIn;
+	document.getElementById("idLogOut").onclick = logOut;	
+	document.getElementById("idPlay").onclick = doSomething;	
     }
 }
 
-socket = {
+var socket;
+client = {
     _ws: null,
     load: function() {
-        var location = 'ws://localhost:3400'
+        var location = 'ws://localhost:3400';
         this._ws = new WebSocket(location);
+	socket = this._ws;
         this._ws.onopen = this._onopen;
         this._ws.onmessage = this._onmessage;
         this._ws.onclose = this._onclose;
@@ -20,13 +25,60 @@ socket = {
 
     _onopen: function() {
         alert('Socket ouvert !');
-        this.send("Hello dude");
     },
     _onclose: function() {
         alert('Socket fermé !');
         this._ws = null;
     },
     _onmessage: function(m) {
-        alert('Je viens de recevoir un message : ' + m.data);
+        alert(m.data);
     }
+
+    /*		
+    sendMessage() {
+	//get a JSONText just for example
+        var JSONObject = { 'action' : 'login', 
+			   'params' : {'name' : 'aduc', 'pass' : ''}
+			 };
+	var JSONText = JSON.stringify(JSONObject);
+	this.send(JSONText);	
+	alert(JSONText);
+    }
+    */   
+  
 }
+
+function logIn(){
+	//get a JSONText just for example
+        var JSONObject = { 'action' : 'login', 
+			   'params' : {'name' : 'aduc', 'pass' : ''}
+			 };
+	
+	var JSONText = JSON.stringify(JSONObject);
+	sendMessage(JSONText);
+}
+
+function logOut(){
+	//get a JSONText just for example
+        var JSONObject = { 'action' : 'logout', 
+			   'params' : {'name' : 'aduc', 'pass' : ''}
+			 };
+	
+	var JSONText = JSON.stringify(JSONObject);
+	sendMessage(JSONText);
+}
+
+function doSomething(){
+	//get a JSONText just for example
+        var JSONObject = { 'action' : 'move', 
+			   'params' : {'direction' : 'left', 'distance' : '2'}
+			 };
+	
+	var JSONText = JSON.stringify(JSONObject);
+	sendMessage(JSONText);
+
+}
+function sendMessage(JSONText) {
+	
+	client._ws.send(JSONText);
+}   
