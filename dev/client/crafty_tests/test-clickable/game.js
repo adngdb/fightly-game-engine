34d96d28554d44d26916c370235a74217f51cb12
AdrianGaudebert <adrian@gaudebert.fr>
2011-01-20 15:37:38 +0100

    Corrected the bug :) :)

--- dev/client/crafty_tests/test-clickable/game.js
+++ dev/client/crafty_tests/test-clickable/game.js
@@ -12,22 +12,22 @@ $(document).ready(function() {
     Crafty.sprite(128, "images/minotaur.png", {
         mino_left: [0, 0, 1, 1],
     });
-	
+
     //build the map
-	var cellClickMap = new Crafty.polygon([32,16], [64,32], [32,48], [0,32]);
-    for (var j = -1; j < 22; j++)
+    var cellClickMap = function() { return new Crafty.polygon([32,16], [64,32], [32,48], [0,32]); };
+    for (var j = 0; j < 22; j++)
     {
         for (var i = 0; i < 9; i++)
         {
             var x = i * 64 + ( (j % 2) * 32),
                 y = j * 16;
-				
+
             var tile = Crafty.e();
             var grass = "grass" + ( ( Math.floor(Math.random() * 4 ) ) + 1 );
             //alert(grass);
             tile.addComponent("2D, DOM, clickable, " + grass); // ---------
             tile.attr({x: x, y: y, w: 64, h: 64})
-				.clickable(cellClickMap, function () { alert("coucou"); });
+                .clickable(cellClickMap(), function () { alert("Hallo, ich bin "+this.x+", "+this.y); });
         }
     }
 
