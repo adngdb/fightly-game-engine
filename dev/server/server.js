76bab3521b5b0ec44b33eedd01c230588f433cc6
AdrianGaudebert <adrian@gaudebert.fr>
2011-01-25 11:28:08 +0100

    Little refactoring.

--- dev/server/server.js
+++ dev/server/server.js
@@ -1,4 +1,11 @@
-var ge = require("./lib/game-engine.js"),
-    wge = new ge.WGE();
+/***********************************************************************
+ *
+ * Fightly - Web Game Engine
+ * http://fightly.com
+ *
+ **********************************************************************/
 
-wge.start();
+var gameEngine_ = require("./lib/game-engine.js"),
+    ge = new gameEngine_.GameEngine();
+
+ge.start();
