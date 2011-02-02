f0718291d9e76d88b28925da3682ce034d38764f
ducnguyen272 <ducnguyen272@gmail.com>
2011-02-02 09:41:48 +0100

    suprimer actionManager.server dans la classe Server

--- dev/server/lib/network/server.js
+++ dev/server/lib/network/server.js
@@ -7,8 +7,6 @@ var sys = require("sys"),
 
 exports.Server = function() {
     this.server = http.createServer(function(req, res){});
-    //this.actionManager = new actionManager.ActionManager(this.server);
-
     this.socket = io.listen(this.server);
 	
     var me = this;
@@ -29,35 +27,3 @@ exports.Server.prototype = {
     }
 }
 
-/*
-// Require HTTP module (to start server) and Socket.IO
-http = require('http'), io = require('socket.io');
-
-// Start the server at port 8080
-var server = http.createServer(function(req, res){ 
-
-  // Send HTML headers and message
-  res.writeHead(200,{ 'Content-Type': 'text/html' }); 
-  res.end('<h1>Hello Socket Lover!</h1>');
-});
-server.listen(8080);
-
-// Create a Socket.IO instance, passing it our server
-var socket = io.listen(server);
-
-// Add a connect listener
-socket.on('connection', function(client){ 
-	
-  client.send('This is a message from the server!');  
-
-  // Success!  Now listen to messages to be received
-  client.on('message',function(event){ 
-    console.log('Received message from client: ' + event);
-    client.send("hello " + client.sessionId);
-  });
-  client.on('disconnect',function(){    
-    console.log('Server has disconnected');
-  });
-
-});
-*/
