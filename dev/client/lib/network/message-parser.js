f8614e1e369c1d918cf36934264e4130bfda9501
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-03 22:50:34 +0100

    Adding functionnalities.

--- dev/client/lib/network/message-parser.js
+++ dev/client/lib/network/message-parser.js
@@ -12,13 +12,40 @@
  *
  * @author Adrian Gaudebert - adrian@gaudebert.fr
  */
-MessageParser = function() {
+MessageParser = function(ge) {
+    this.ge = ge;
 };
 
 MessageParser.prototype = {
 
     parse: function(message) {
-        return JSON.parse(message);
+        var obj = JSON.parse(message);
+        switch (obj.type) {
+            case "query":
+                this.parseQuery(obj.data);
+                break;
+            case "action":
+                this.parseAction(obj.data);
+                break;
+            case "data":
+                this.parseData(obj.data);
+                break;
+            default:
+                throw "Unknown message type"; // TODO: using an object of type Exception
+        }
+        return this;
+    },
+
+    parseQuery: function(data) {
+        return this;
+    },
+
+    parseAction: function(data) {
+        return this;
+    },
+
+    parseData: function(data) {
+        return this;
     },
 
 };
