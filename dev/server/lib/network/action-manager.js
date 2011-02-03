9d0506989f572f8c28c5c02d8d64d14deda6f18b
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-03 22:48:13 +0100

    Removing unused code, correcting some bugs.

--- dev/server/lib/network/action-manager.js
+++ dev/server/lib/network/action-manager.js
@@ -1,41 +1,8 @@
-exports.ActionManager = function() { 
-    
+exports.ActionManager = function() {
+
 };
 
 exports.ActionManager.prototype = {
-
-    parseMessage : function(msg){
-        return JSON.parse(msg);
-    },
-    
-    manageMessage : function(connection, msg) {
-    	var objectJSON = this.parseMessage(msg);
-
-    	if(objectJSON.action == "login") {
-	    //create a new player whose username is objectJSON.params.name
-	    connection.send("Hi " + objectJSON.params.name);
-    	}
-	else if(objectJSON.action == "logout") {
-	    //closed connection
-	    connection.send("Goodbye " + objectJSON.params.name);
-	    //connection.close();
-    	}
-    	else {
-	    //play game: move,...
-	    this.playGame(connection, objectJSON);
-    	}
-    },
-    
-    playGame : function(conn, objectJSON) {
-
-    	//check action with rule base
-
-    	//excute action (return JSON message)
-    	console.log(conn.sessionId + " play: " + objectJSON.action + " "
-        		+ objectJSON.params.direction + " "
-	        	+ objectJSON.params.distance + "cells");
-    },
-
 
     /**
      *
@@ -53,7 +20,7 @@ exports.ActionManager.prototype = {
     createQuery: function(responseType, responseData) {
         var data = {};
         data.response_type = responseType;
-	data.data = responseData;
+    data.data = responseData;
 
         return this.createMessage("query", data);
     },
@@ -81,71 +48,19 @@ exports.ActionManager.prototype = {
      */
 
     createAuthentificationQuery: function() {
-        createQuery("login"); 
-    },    
+        this.createQuery("login");
+    },
 
     createConfirmationData: function(object_data){
-	createData("new", "player", object_data);
+        this.createData("new", "Player", object_data);
     },
-    
+
     createJoinAction: function(){
-	createAction();
+        this.createAction();
     },
 
-    createPartieData: function(object_data){
-	createData("new", "game", object_data);
+    createGameData: function(object_data){
+        this.createData("new", "Game", object_data);
     },
 
 }
-
-/*
-exports.ActionManager.parseMessage = function(msg){
-    return JSON.parse(msg);
-}
-
-exports.ActionManager.manageMessage = function(connection, msg) {
-    var objectJSON = exports.ActionManager.parseMessage(msg);
-
-    if(objectJSON.action == "login") {
-        //create a new player whose username is objectJSON.params.name
-	connection.send("Hi " + objectJSON.params.name);
-    }
-    else if(objectJSON.action == "logout") {
-        //closed connection
-        connection.send("Goodbye " + objectJSON.params.name);
-        //connection.close();
-    }
-    else {
-        //play game: move,...
-        exports.ActionManager.playGame(connection, objectJSON);
-    }
-}
-
-
-exports.ActionManager.playGame = function(conn, objectJSON) {    
-
-    //check action with rule base
-
-    //excute action (return JSON message)
-    console.log(conn.sessionId + " play: " + objectJSON.action + " "
-        + objectJSON.params.direction + " "
-        + objectJSON.params.distance + "cells");
-}
-
-
-exports.ActionManager.moveLeft = function() {
-
-}
-
-exports.ActionManager.moveRight = function() {
-
-}
-
-exports.ActionManager.moveUp = function() {
-
-}
-
-exports.ActionManager.moveDown = function() {
-
-}
-*/
