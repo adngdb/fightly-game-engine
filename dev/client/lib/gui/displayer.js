1e4da0402e9ca4587a3f6e1e82e4690f52e2bebb
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-05 21:29:59 +0100

    Adding TODOs.

--- dev/client/lib/gui/displayer.js
+++ dev/client/lib/gui/displayer.js
@@ -12,21 +12,29 @@
  *
  * @author Adrian Gaudebert - adrian@gaudebert.fr
  */
-Displayer = function() {
+Displayer = function(world) {
+    this.world = world;
+
     this.loggerElt = $('#log');
 };
 
 Displayer.prototype = {
 
     log: function(msg) {
+        // TODO
+        // Check if the logging is activated
         this.loggerElt.append('<li>'+msg+'</li>');
     },
 
     getUserLogin: function() {
+        // TODO
+        // Use something better to get the credentials
         return prompt("Username", "player");
     },
 
     getGameId: function() {
+        // TODO
+        // Check the variable's name
         return window.location.search.substring(1).split("=")[1];
     },
 
