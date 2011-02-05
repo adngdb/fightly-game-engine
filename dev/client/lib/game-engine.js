9dede8f9b283f9297228c48e0ffe8f930ed775b5
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-05 18:17:44 +0100

    Adding some functions about triggers.

--- dev/client/lib/game-engine.js
+++ dev/client/lib/game-engine.js
@@ -13,22 +13,51 @@
  * @author Adrian Gaudebert - adrian@gaudebert.fr
  */
 GameEngine = function() {
-    this.com = null;
-    this.mb = null;
+    this.comManager = null;
+    this.messageBuilder = null;
+
+    this._handlers = [];
 };
 
 GameEngine.prototype = {
 
+    /**
+     * Initialize the GameEngine, instanciate tools.
+     * @return this
+     */
     init: function() {
-        this.mb = new MessageBuilder();
+        this.messageBuilder = new MessageBuilder();
 
-        this.com = new ComManager(this);
-        this.com.init();
+        this.comManager = new ComManager(this);
         return this;
     },
 
+    /**
+     * Start the GameEngine.
+     * @return this
+     */
     start: function() {
+        this.comManager.init();
         return this;
+    },
+
+    trigger: function(event, data) {
+        this._handlers[event].call(this, data);
+        return this;
+    },
+
+    bind: function(event, callback) {
+        this._handlers[event] = callback;
+        return this;
+    },
+
+    unbind: function(event) {
+        this._handlers.splice(event, 1);
+    },
+
+    onAuthenticationQuery: function() {
+        var login = prompt("Username", "player");
+        this.comManager.send(this.messageBuilder.createAuthenticationLogin(login));
     },
 
 };
