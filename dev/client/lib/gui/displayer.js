e8270c75e8e3f1355f12906a6f342778d9bd5712
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-05 18:26:59 +0100

    First commit.

---
+++ dev/client/lib/gui/displayer.js
@@ -0,0 +1,19 @@
+/***********************************************************************
+ *
+ * Fightly - Web Game Engine
+ * http://fightly.com
+ *
+ * License: see LICENSE.txt
+ *
+ **********************************************************************/
+
+/**
+ * Class Displayer
+ *
+ * @author Adrian Gaudebert - adrian@gaudebert.fr
+ */
+Displayer = function() {
+};
+
+Displayer.prototype = {
+};
