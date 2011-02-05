045d1b82fc519832963e9938b4762e10328d94b4
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-05 14:20:09 +0100

    Instanciate a MessageBuilder instead of the ActionManager.

--- dev/server/lib/network/com-manager.js
+++ dev/server/lib/network/com-manager.js
@@ -12,29 +12,31 @@ var sys = require("sys"),
     io = require('socket.io'),
 
     client_ = require("./client"),
-    am_ = require("./message-builder");
+    messageBuilder_ = require("./message-builder");
 
-exports.ComManager = function() {
+exports.ComManager = function(gameEngine) {
+
+    this.gameEngine = gameEngine;
+
     this.server = http.createServer(function(req, res){});
     this.socket = io.listen(this.server);
 
-    this.actionManager = new am_.ActionManager();
-
-    var me = this;
     this.socket.on('connection', function(conn){
-    me.onConnect(conn);
-    });
+        this.onConnect(conn);
+    }.bind(this));
+
 }
 
 exports.ComManager.prototype = {
+
     listen : function(port){
-    this.server.listen(port);
-    sys.log("Server created. Listening on port " + port + ".");
+        this.server.listen(port);
+        sys.log("Server created. Listening on port " + port + ".");
     },
 
     onConnect : function(connection) {
-    sys.log("New connection: " + connection.sessionId);
+        sys.log("New connection: " + connection.sessionId);
         new client_.Client(connection, this);
-    }
-}
+    },
 
+}
