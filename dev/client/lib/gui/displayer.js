fbac7c7db3bdc0925a7e9bcbf75d58fa4319dbcf
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-08 11:23:57 +0100

    invalidate & displayState

--- dev/client/lib/gui/displayer.js
+++ dev/client/lib/gui/displayer.js
@@ -40,11 +40,31 @@ Displayer.prototype = {
         return window.location.search.substring(1).split("=")[1];
     },
 
+    invalidate: function() {
+        if (this.world.game != null) {
+            this.displayGame();
+        }
+        else {
+            this.displayState();
+        }
+    },
+
+    displayState: function() {
+        // TODO
+    },
+
     displayGame: function() {
+        alert('display');
         var state = this.world.game.state;
+        var players = this.world.game.players;
         if (state == "waiting") {
             this.gameElt.empty().append('<h1>Waiting for opponents...</h1>');
         }
+        this.gameElt.append('<ul>');
+        for (var i = 0; i < players.length; i++) {
+            this.gameElt.append('<li>'+ players[i].login +'</li>');
+        }
+        this.gameElt.append('</ul>');
     },
 
 };
