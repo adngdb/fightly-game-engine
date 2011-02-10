2be836744b403b400f60dab1d856f479cc53c965
AdrianGaudebert <adrian@gaudebert.fr>
2011-02-11 00:35:13 +0100

    Now displays the map, and (badly) units.

--- dev/client/lib/gui/game-displayer.js
+++ dev/client/lib/gui/game-displayer.js
@@ -19,30 +19,50 @@ function GameDisplayer(world) {
     this.height = 600;
     this.spriteSize = 64;
 
-    this.mapSprite = 'assets/images/map.png';
+    this.mapSprite = './assets/images/map.png';
+    this.unitSprite = './assets/images/unit.png';
 
     this.iso = Crafty.isometric.init(this.spriteSize);
 };
 
 GameDisplayer.prototype = {
     init: function() {
+        var map = this.world.game.map;
+
+        this.width = map.width * this.spriteSize + this.spriteSize / 2;
+        this.height = map.height * this.spriteSize / 2;
+
         Crafty.init(50, this.width, this.height);
+
+        this.createSprites();
     },
 
     createSprites: function() {
-        Crafty.sprite(this.spriteSize, {
-            grass1: [0, 0, 1, 1],
-            grass2: [1, 0, 1, 1],
-            grass3: [2, 0, 1, 1],
-            grass4: [3, 0, 1, 1],
+        // Map sprite
+        Crafty.sprite(this.spriteSize, this.mapSprite, {
+            plains:     [0, 0, 1, 1],
+            swamp:      [1, 0, 1, 1],
+            mountain:   [2, 0, 1, 1],
+        });
+
+        // Unit sprite
+        Crafty.sprite(this.spriteSize, this.unitSprite, {
+            unit: [0, 0, 1, 1],
         });
     },
 
     display: function() {
+        this.displayMap()
+            .displayUnits();
+    },
 
-        for (var y = 0; y < this.height / this.spriteSize * 3; y++) {
-            for (var x = 0; x < this.width / this.spriteSize - 1; x++) {
-                var tile = Crafty.e('2D, DOM, grass1, mouse')
+    displayMap: function() {
+        var map = this.world.game.map;
+
+        for (var y = 0; y < map.height; y++) {
+            for (var x = 0; x < map.width; x++) {
+                var cell = map.cells[x][y];
+                var tile = Crafty.e('2D, DOM, mouse, '+cell.type)
                     .areaMap([32,16], [64,32], [32,48], [0,32])
                     .bind("click", function() {
                         this.destroy();
@@ -50,6 +70,27 @@ GameDisplayer.prototype = {
                 this.iso.place(x, y, 0, tile);
             }
         }
-
+        return this;
     },
+
+    displayUnits: function() {
+        var players = this.world.game.players,
+            i = 0,
+            l = players.length;
+
+        for (; i < l; i++) {
+            var pl = players[i],
+                units = pl.units;
+
+            var j = 0,
+                ul = units.length;
+
+            for (; j < ul; j++) {
+                var unit = units[j];
+                var unitSprite = Crafty.e('2D, DOM, mouse, unit');
+                this.iso.place(unit.cell.x, unit.cell.y, 1, unitSprite);
+            }
+        }
+    },
+
 };
