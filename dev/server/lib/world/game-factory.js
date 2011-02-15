bbf6fe5b2ed5f47b313cd971b814bba2afda1568
ducnguyen272 <ducnguyen272@gmail.com>
2011-02-15 11:47:45 +0100

    configurations

--- dev/server/lib/world/game-factory.js
+++ dev/server/lib/world/game-factory.js
@@ -25,17 +25,18 @@ exports.GameFactory.prototype = {
     create: function(id) {
 
         var myGame = new game_.Game();
-
+	
         myGame.id = id;
         myGame.map = this.mapFactory.create();
         myGame.playerFactory = this.playerFactory;
         myGame.mapFactory = this.mapFactory;
         myGame.unitFactory = this.unitFactory;
-
+        myGame.nbMaxPlayers = this.config.nbMaxPlayers; 
+        myGame.nbMaxTurns = this.config.nbMaxTurns;
         return myGame;
     },
 
-    setConfig : function(nbMaxPlayer) {
+    setConfig : function(nbMaxPlayers, nbMaxTurns) {
 
         this.config.nbMaxPlayers = nbMaxPlayers;
         this.config.nbMaxTurns = nbMaxTurns;
