6b4e851ae604e5db6271f64e4986ea336dd11686
Youness Hamri <youness_hamri@yahoo.fr>
2011-02-15 11:55:19 +0100

    game Factory

--- dev/server/lib/world/game-factory.js
+++ dev/server/lib/world/game-factory.js
@@ -15,8 +15,9 @@ exports.GameFactory = function() {
     this.cellFactory = null;
     this.unitFactory = null;
     this.config = {
-        "nbMaxPlayers": 2,
-        "nbMaxTurns" : 200
+        "nbMaxPlayers": 3,
+        "nbMaxTurns" : 200,
+        "turnDuration" : 20, //second
     };
 }
 
@@ -25,21 +26,23 @@ exports.GameFactory.prototype = {
     create: function(id) {
 
         var myGame = new game_.Game();
-	
+
         myGame.id = id;
         myGame.map = this.mapFactory.create();
         myGame.playerFactory = this.playerFactory;
         myGame.mapFactory = this.mapFactory;
         myGame.unitFactory = this.unitFactory;
-        myGame.nbMaxPlayers = this.config.nbMaxPlayers; 
+        myGame.nbMaxPlayers = this.config.nbMaxPlayers;
         myGame.nbMaxTurns = this.config.nbMaxTurns;
+        myGame.turnDuration = this.config.turnDuration;
         return myGame;
     },
 
-    setConfig : function(nbMaxPlayers, nbMaxTurns) {
+    setConfig : function(nbMaxPlayers, nbMaxTurns, turnDuration) {
 
         this.config.nbMaxPlayers = nbMaxPlayers;
         this.config.nbMaxTurns = nbMaxTurns;
+        this.config.turnDuration = turnDuration;
 
     },
 
