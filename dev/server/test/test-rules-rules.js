08c03eec79226762bfa9c9694f48349387a1ef40
maximecolin <binome.lyon@gmail.com>
2011-02-18 16:58:42 +0100

    Update of the unit test for rules and action-manager

--- dev/server/test/test-rules-rules.js
+++ dev/server/test/test-rules-rules.js
@@ -12,8 +12,13 @@ var sys = require('sys') ;
 var rules_ = require('lib/rules/rules.js') ;
 
 exports['load'] = function (test) {
-    var r = new rules_.Rules() ;
+    var r = new rules_.Rules(null) ;
     r.load('rules.json') ;
     test.done() ;
 };
+
+exports['configureFactories'] = function (test) {
+	
+	test.done() ;
+}
 
