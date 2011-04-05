7b1942c523ea9c9c33c2fee299e5d3e7899192e6
AdrianGaudebert <adrian@gaudebert.fr>
2011-04-05 21:24:28 +0200

    For server doc, -S option is not needed anymore thanks to classes rewriting.

--- generate_js_doc.sh
+++ generate_js_doc.sh
@@ -5,4 +5,4 @@
 java -jar docs/tools/jsdoc_toolkit/jsrun.jar docs/tools/jsdoc_toolkit/app/run.js -a -t=docs/tools/jsdoc_toolkit/templates/jsdoc -d=docs/api/client/ dev/client/lib/ dev/client/lib/*/
 
 #Server code
-java -jar docs/tools/jsdoc_toolkit/jsrun.jar docs/tools/jsdoc_toolkit/app/run.js -a -S -t=docs/tools/jsdoc_toolkit/templates/jsdoc -d=docs/api/server/ dev/server/lib/ dev/server/lib/*/
+java -jar docs/tools/jsdoc_toolkit/jsrun.jar docs/tools/jsdoc_toolkit/app/run.js -a -t=docs/tools/jsdoc_toolkit/templates/jsdoc -d=docs/api/server/ dev/server/lib/ dev/server/lib/*/
