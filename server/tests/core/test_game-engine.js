bdf3cc2f1fdca9f0b295bace8837d8b7da81f4fe
Adrian Gaudebert <adrian@gaudebert.fr>
2012-02-10 14:27:05 +0100

    Added a unit test file for the GameEngine class.

---
+++ server/tests/core/test_game-engine.js
@@ -0,0 +1,23 @@
+/**
+ * Test cases for core.GameEngine class.
+ *
+ * Requires node.js and it's nodeunit module.
+ * To run those tests: nodeunit tests
+ *
+ * @author Adrian Gaudebert - adrian@gaudebert.fr
+ */
+
+var GameEngine = require('../../lib/core/game-engine');
+
+
+exports['inherits'] = function (test) {
+    var myGE = new GameEngine();
+
+    test.ok(typeof myGE.get == 'function');
+    test.ok(typeof myGE.addComponent == 'function');
+    test.ok(typeof myGE.createEntity == 'function');
+    test.ok(typeof myGE.loadActions == 'function');
+    test.ok(typeof myGE.actions == 'object');
+
+    test.done();
+}
