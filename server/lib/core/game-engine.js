b0dab02755af4a899775af32bdaf5fd22c6f8b33
Adrian Gaudebert <adrian@gaudebert.fr>
2012-02-16 16:32:05 +0100

    Added modules and components loading. Added unit tests.

--- server/lib/core/game-engine.js
+++ server/lib/core/game-engine.js
@@ -7,10 +7,14 @@
  *
  *****************************************************************************/
 
-var util = require('util');
+var util = require('util')
+    , fs = require('fs')
+    , path = require('path')
+    ;
 
-var cem = require('../../vendor/component-entity/component-entity-manager'),
-    am = require('../../vendor/action-manager/action-manager');
+var cem = require('../../vendor/component-entity/component-entity-manager')
+    , am = require('../../vendor/action-manager/action-manager')
+    ;
 
 /**
  * Class GameEngine
@@ -35,6 +39,76 @@ GameEngine.prototype = {
         // Load all components
         // Start network
         // Listen to events
+    },
+
+    _loadComponents: function() {},
+    _loadCoreComponents: function() {
+        var game = require('./game.js');
+    },
+
+    /**
+     * Load and register all modules' components into this GameEngine.
+     *
+     * @param pathToModules String, directory where all the modules live.
+     * @return this.
+     */
+    _loadModulesComponents: function(pathToModules) {
+        var modules = this._getModulesList(pathToModules)
+            , module
+            , files
+            , pathToFile
+            , stat
+            , component
+            ;
+
+        for (m in modules) {
+            module = modules[m];
+
+            // List all files in this directory
+            files = fs.readdirSync(module);
+            files.sort();
+            for (f in files) {
+                file = files[f];
+                pathToFile = path.join(module, file);
+                stat = fs.statSync(pathToFile);
+
+                if (stat.isFile() && file.lastIndexOf('.js') === file.length - 3 && file !== 'actions.js') {
+                    // It's a component file, let's load it
+                    components = require('../../' + pathToFile); // Node.js sad hack
+                    for (c in components) {
+                        // Add this component to the GameEngine
+                        this.c(c, components[c]);
+                    }
+                }
+            }
+        }
+
+        return this;
+    },
+
+    /**
+     * Return a list of all module directories in pathToModules. Return an
+     * empty list if pathToModules does not exist.
+     *
+     * @param pathToModules String, directory where all the modules live.
+     * @return A list of paths to module folders, or an empty list.
+     */
+    _getModulesList: function(pathToModules) {
+        var modules;
+
+        pathToModules = path.normalize(pathToModules);
+        if (!path.existsSync(pathToModules)) {
+            util.debug("The modules directory '" + pathToModules + "' doesn't exist");
+            return [];
+        }
+
+        modules = fs.readdirSync(pathToModules);
+        modules.sort();
+        for (m in modules) {
+            modules[m] = path.join(pathToModules, modules[m]);
+        }
+
+        return modules;
     },
 
     createGame: function() {
--- server/tests/core/test_game-engine.js
+++ server/tests/core/test_game-engine.js
@@ -6,6 +6,7 @@
  *
  * @author Adrian Gaudebert - adrian@gaudebert.fr
  */
+var path = require('path');
 
 var GameEngine = require('../../lib/core/game-engine');
 
@@ -18,6 +19,41 @@ exports['inherits'] = function (test) {
     test.ok(typeof myGE.createEntity == 'function');
     test.ok(typeof myGE.loadActions == 'function');
     test.ok(typeof myGE.actions == 'object');
+
+    test.done();
+}
+
+exports['get-modules-list'] = function (test) {
+    var myGE = new GameEngine(),
+        pathToModules = 'tests/modules',
+        modules = myGE._getModulesList(pathToModules),
+        modulesExpected;
+
+    test.ok(Array.isArray(modules));
+    test.equal(modules.length, 3); // there are 3 folders in tests/modules/
+
+    modulesExpected = [
+        path.join(pathToModules, 'core'),
+        path.join(pathToModules, 'map'),
+        path.join(pathToModules, 'unit'),
+    ];
+    test.deepEqual(modules, modulesExpected);
+
+    test.done();
+}
+
+exports['load-modules-components'] = function (test) {
+    var myGE = new GameEngine(),
+        pathToModules = 'tests/modules';
+
+    myGE._loadModulesComponents(pathToModules);
+
+    var compList = myGE.getComponentsList();
+    test.equal(typeof compList, 'object');
+    test.notEqual(compList.indexOf('Position'), -1);
+    test.notEqual(compList.indexOf('Cell'), -1);
+    test.notEqual(compList.indexOf('Map'), -1);
+    test.notEqual(compList.indexOf('Unit'), -1);
 
     test.done();
 }
