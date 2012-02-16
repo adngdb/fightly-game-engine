cc4269a0e777a16843b0373d7b8e6c84d76ea299
Adrian Gaudebert <adrian@gaudebert.fr>
2012-02-16 12:28:59 +0100

    Removed obsolete unit tests.

--- server/test/test-game-engine.js
+++
@@ -1,9 +0,0 @@
-var sys = require('sys'),
-    gameEngine_ = require('../lib/game-engine.js');
-
-exports['log'] = function (test) {
-    sys.log("Test");
-    var ge = new gameEngine_.GameEngine();
-    ge.start();
-    test.done();
-};
