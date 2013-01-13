08e43d84a4b6a5306229663ab780c2061b6736be
Adrian Gaudebert <adrian@gaudebert.fr>
2013-01-13 19:46:34 +0100

    First tests and unit tests.

---
+++ fightly/client/test/test_fightly.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var F = require('../src/fightly');
+
+describe('Fightly', function () {
+    describe('#loadConfig()', function () {
+        var fightly = F();
+
+        fightly.loadConfig();
+    })
+});
