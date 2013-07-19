580f7aa9c78e138ddac142577cbdd3c85e28194e
Adrian Gaudebert <adrian@gaudebert.fr>
2013-07-19 19:50:47 +0200

    More test coverage for core actions.

--- fightly/server/tests/core/test_actions.js
+++ fightly/server/tests/core/test_actions.js
@@ -17,12 +17,20 @@ exports['join-game'] = function (test) {
     var myGE = new GameEngine(config.core);
     myGE.init();
 
-    var g = myGE.e('Game'),
-        p = myGE.e('Player');
+    var g = myGE.e('Game');
+    var p1 = myGE.e('Player');
+    var p2 = myGE.e('Player');
+    var p3 = myGE.e('Player');
 
-
-    test.ok(myGE.actions.core.joinGame(g, p));
+    test.ok(myGE.actions.core.joinGame(g, p1));
     test.equal(g.players.length, 1);
+
+    test.ok(myGE.actions.core.joinGame(g, p2));
+    test.equal(g.players.length, 2);
+
+    // Test the maximum number of players cannot be passed
+    test.ok(!myGE.actions.core.joinGame(g, p3));
+    test.equal(g.players.length, 2);
 
     test.done();
 }
@@ -31,19 +39,20 @@ exports['next-turn'] = function (test) {
     var myGE = new GameEngine(config.core);
     myGE.init();
 
-    var g = myGE.e('Game'),
-        p = myGE.e('Player');
+    var g = myGE.e('Game');
+    var p1 = myGE.e('Player');
+    var p2 = myGE.e('Player');
 
-    g.players.push(p);
-    g.activePlayer = p;
-    p.inGame = g;
+    g.players.push(p1);
+    g.players.push(p2);
+    g.activePlayer = p1;
+    p1.inGame = p2.inGame = g;
 
-    test.equal(typeof g, 'object');
     test.equal(g.currentTurn, 0);
 
-    test.ok(myGE.actions.core.nextTurn(g, p));
-
+    test.ok(myGE.actions.core.nextTurn(g, p1));
     test.equal(g.currentTurn, 1);
+    test.equal(g.activePlayer.id, p2.id);
 
     test.done();
 }
